84b55a0e6b5788bcfdc60cdff5121a684131d07b
Pudge <hook.pudge@foxmail.com>
2024-06-22 22:09:24 +0800

    add lazy method

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,15 @@ import GeekLayout from "@/pages/Layout";
 import P404 from "@/pages/404";
 import Login from "@/pages/Login";
 import AuthRoute from "@/components/AuthRoute";
-import Home from "@/pages/Home";
-import Article from "@/pages/Article";
-import Publish from "@/pages/Publish";
+//import Home from "@/pages/Home";
+//import Article from "@/pages/Article";
+//import Publish from "@/pages/Publish";
+import { Suspense, lazy } from "react";
+
+//路由懒加载
+const Home = lazy(() => { return import('@/pages/Home') })
+const Article = lazy(() => { return import('@/pages/Article') })
+const Publish = lazy(() => { return import('@/pages/Publish') })
 
 const router = createBrowserRouter([
     {
@@ -16,16 +22,16 @@ const router = createBrowserRouter([
         children: [
             {
                 path: '/home',//如果首页展示请用index
-                element: <Home />
+                element: <Suspense fallback={'加载中'}><Home /></Suspense>
 
             },
             {
                 path: '/article',
-                element: <Article />
+                element: <Suspense fallback={'加载中'} ><Article /></Suspense>
             },
             {
                 path: '/publish',
-                element: <Publish />
+                element: <Suspense fallback={'加载中'}><Publish /></Suspense>
             }
         ]
     },
